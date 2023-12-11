c98ed40dd5ef717e554b279de84ce8a3168b6a70
Sujar Henry <skinghenry@gmail.com>
2023-12-11 16:36:58 -0500

    adding all files

---
+++ task_manager/Controllers/TaskController.cs
@@ -0,0 +1,159 @@
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Threading.Tasks;using Microsoft.AspNetCore.Mvc;namespace task_manager.Controllers{    using Pages;    [ApiController]    [Route("[action]")]    public class TaskControllers : ControllerBase    {        public string KnockKnock()        {            return "Whos There";        }
+
+
+        [Route("{id:int}/{newDescription}")]        public string ChangeDescriptionById(int id, string newDescription)        {            var task = IndexModel.tasks.FirstOrDefault(t => t.Id == id);            task.Description = newDescription;            return "Changed description of task";        }
+        
+
+
+        [Route("{description}")]
+        public string SearchTaskByDescription(string description)
+        {
+            var task = IndexModel.tasks.FirstOrDefault(t => t.Description.ToLower() == description.ToLower());
+
+            if (task != null)
+            {
+                return $"Task found: {task.Id} - {task.Description} - {task.DueDate} - {task.IsCompleted} - {task.CompletionDate}";
+            }
+
+            return "Task was not found";
+        }        [Route("{id:int}")]        public string SearchTaskById(int id)
+        {
+            var task = IndexModel.tasks.FirstOrDefault(t => t.Id == id);
+
+            return task.Id.ToString() + " " + task.Description + " " + task.DueDate + " " + task.IsCompleted + " " + task.CompletionDate;
+        }        public string GetAllTasks()
+        {
+            string final = "";
+            for (int i = 1; i <= IndexModel.tasks.Count; i++)
+            {
+                var task = IndexModel.tasks.FirstOrDefault(t => t.Id == i);
+
+                string task_string = "Task" + i + "-" + task.Id.ToString() + " " + task.Description + " " + task.DueDate + " " + task.IsCompleted + " " + task.CompletionDate + "\n";
+
+                final += task_string;
+
+            }
+            return final;
+        }        public string GetAllCompletedTasks()
+        {
+            string final = "";
+            for (int i = 1; i <= IndexModel.tasks.Count; i++)
+            {
+                var task = IndexModel.tasks.FirstOrDefault(t => t.Id == i);
+
+                string task_string = "Task" + i + "-" + task.Id.ToString() + " " + task.Description + " " + task.DueDate + " " + task.IsCompleted + " " + task.CompletionDate + "\n";
+                if (task.IsCompleted)
+                {
+                    final += task_string;
+                }
+
+            }
+            return final;
+        }
+
+        [Route("{id:int}")]
+        public string MarkTaskAsCompleted(int id)
+        {
+            var task = IndexModel.tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task != null)
+            {
+                task.IsCompleted = true;
+                task.CompletionDate = DateTime.Now;
+                return "Task marked as complete";
+            }
+
+            return "Task was not found";
+        }
+
+        //Sorting
+
+        public string SortTasksByCompletedStatus()
+        {
+            var sortedTasks = IndexModel.tasks.OrderBy(t => t.IsCompleted).ToList();
+
+            string result = "Sorted Tasks by Completed Status:\n";
+
+            foreach (var task in sortedTasks)
+            {
+                result += $"{task.Id} - {task.Description} - {task.DueDate} - {task.IsCompleted} - {task.CompletionDate}\n";
+            }
+
+            return result;
+        }
+
+
+        public string SortTasksByCompletionDate()
+        {
+            var sortedTasks = IndexModel.tasks.OrderBy(t => t.CompletionDate).ToList();
+
+            string result = "Sorted Tasks by Completion Date:\n";
+
+            foreach (var task in sortedTasks)
+            {
+                result += $"{task.Id} - {task.Description} - {task.DueDate} - {task.IsCompleted} - {task.CompletionDate}\n";
+            }
+
+            return result;
+        }
+
+
+        public string SortTasksByDueDate()
+        {
+            var sortedTasks = IndexModel.tasks.OrderBy(t => t.DueDate).ToList();
+
+            string result = "Sorted Tasks by Due Date:\n";
+
+            foreach (var task in sortedTasks)
+            {
+                result += $"{task.Id} - {task.Description} - {task.DueDate} - {task.IsCompleted} - {task.CompletionDate}\n";
+            }
+
+            return result;
+        }
+
+        //Filtering
+        public string FilterByCompletionStatus()
+        {
+            string final = "Filtered Tasks by Completion Status:\n";
+            for (int i = 1; i <= IndexModel.tasks.Count; i++)
+            {
+                var task = IndexModel.tasks.FirstOrDefault(t => t.Id == i);
+
+                string task_string = "Task" + i + "-" + task.Id.ToString() + " " + task.Description + " " + task.DueDate + " " + task.IsCompleted + " " + task.CompletionDate + "\n";
+                if (task.IsCompleted)
+                {
+                    final += task_string;
+                }
+
+            }
+            return final;
+        }
+
+        public string FilterTasksByDueDate()
+        {
+            var sortedTasks = IndexModel.tasks.OrderBy(t => t.DueDate).ToList();
+
+            string result = "Filtered Tasks by Due Date:\n";
+
+            foreach (var task in sortedTasks)
+            {
+                result += $"{task.Id} - {task.Description} - {task.DueDate} - {task.IsCompleted} - {task.CompletionDate}\n";
+            }
+
+            return result;
+        }
+
+        public string FilterTasksByCompletionDate()
+        {
+            var sortedTasks = IndexModel.tasks.OrderBy(t => t.CompletionDate).ToList();
+
+            string result = "Filter Tasks by Completion Date:\n";
+
+            foreach (var task in sortedTasks)
+            {
+                result += $"{task.Id} - {task.Description} - {task.DueDate} - {task.IsCompleted} - {task.CompletionDate}\n";
+            }
+
+            return result;
+        }    }}
---
+++ task_manager/Pages/Index.cshtml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Reflection;
+
+namespace task_manager.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        //Dummy Tasks
+        public static List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem {Id = 1,Description = "Finish my homework",DueDate = new DateTime(2023,12,15,00,00,00),IsCompleted = false},
+            new TaskItem {Id = 2,Description = "Get a pet fish",DueDate = new DateTime(2024,01,08,00,00,00),IsCompleted = false},
+            new TaskItem {Id = 3,Description = "Complete my exam",DueDate = new DateTime(2024,10,12,00,00,00), IsCompleted = false}};
+
+
+        public List<TaskItem> Tasks => tasks;
+
+        //Methods to Remove, add,edit and mark tasks as complete
+        public IActionResult OnPostRemoveTask(int taskId)
+        {
+            var taskToRemove = tasks.FirstOrDefault(t => t.Id == taskId);
+            if (taskToRemove != null)
+            {
+                tasks.Remove(taskToRemove);
+            }
+            return RedirectToPage();
+        }
+        public IActionResult OnPostUpdateTask(int id,string newDescription,DateTime newDueDate,bool newIsCompleted, DateTime? newCompletionDate)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task == null)
+                return NotFound();
+            
+            task.Description = newDescription;
+            task.DueDate = newDueDate;
+            task.IsCompleted = newIsCompleted;
+            task.CompletionDate = newCompletionDate;
+
+            return RedirectToPage();
+        }
+        public IActionResult OnPostAddTask(TaskItem addTask)
+        {
+            addTask.Id = tasks.Count + 1;
+            tasks.Add(addTask);
+            return RedirectToPage();
+        }
+        public IActionResult OnPostMarkCompleted(int taskId)
+        {
+            var taskToComplete = tasks.FirstOrDefault(t => t.Id == taskId);
+            if (taskToComplete != null)
+            {
+                taskToComplete.IsCompleted = true;
+                taskToComplete.CompletionDate = DateTime.Now; 
+            }
+            return RedirectToPage();
+        }
+
+
+
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //Create the task items with getter and setter methods
+        public class TaskItem
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public DateTime DueDate { get; set; }
+            public bool IsCompleted { get; set; }
+            public DateTime? CompletionDate { get; set; }
+        }
+    }
+}
+
